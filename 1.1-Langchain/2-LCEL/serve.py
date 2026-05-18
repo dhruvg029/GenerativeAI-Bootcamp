e0c8da6cf4a717e4a111ee625b6694ecccce0c7d
Dhruv Gupta <dhruvg029@gmail.com>
2026-05-18 13:14:45 +0200

    renamed the folders

---
+++ 1.1-Langchain/2-LCEL/serve.py
@@ -0,0 +1,37 @@
+## Load libraries and packages
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+import os
+
+from langserve import add_routes
+from dotenv import load_dotenv
+load_dotenv()
+
+## Get the Groq API key from environment variable and initialize the model
+groq_api_key = os.getenv("GROQ_API_KEY")
+model = ChatGroq(model = "llama-3.1-8b-instant", groq_api_key = groq_api_key)
+
+## Create prompt template
+system_template = "Translate the following into {language}:"
+prompt_template = ChatPromptTemplate.from_messages([
+    ('system', system_template),
+    ('user', '{text}')
+])
+
+## Create output parser
+parser = StrOutputParser()
+
+## Create chain
+chain = prompt_template | model | parser
+
+## App definition
+app = FastAPI(title = "Langchain Server", version="1.0", description="A simple API server using Langchain runnable interfaces")
+
+## Adding chain routes
+add_routes(app, chain, path="/chain")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port = 8000)
