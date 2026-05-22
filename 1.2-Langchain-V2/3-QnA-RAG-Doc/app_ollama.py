4d9a00d4abd9fd2004da5f849d0e3bd000d01531
Dhruv Gupta <dhruvg029@gmail.com>
2026-05-22 09:30:21 +0200

    added qna document files

---
+++ 1.2-Langchain-V2/3-QnA-RAG-Doc/app_ollama.py
@@ -0,0 +1,101 @@
+## Implement RAG for Document Q&A
+## Import necessary libraries
+import streamlit as st
+import os
+import time
+from langchain_groq import ChatGroq
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_classic.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+
+from dotenv import load_dotenv
+load_dotenv()
+
+## load the GROQ API Key
+os.environ['GROQ_API_KEY'] = os.getenv("GROQ_API_KEY")
+groq_api_key = os.getenv("GROQ_API_KEY")
+
+## Import the LLM Groq
+llm = ChatGroq(
+    model = "llama-3.3-70b-versatile",
+    api_key = groq_api_key,
+    temperature = 0
+)
+
+## Create the prompt template
+prompt = ChatPromptTemplate.from_template(
+    """
+    Answer the questions based on the provided context only.
+    Please provide the most accurate respone based on the question
+    <context>
+    {context}
+    <context>
+    Question:{input}
+
+    """
+)
+
+## Function to create the vector embeddings
+def create_vector_embedding():
+    if "vectors" not in st.session_state:
+        ## Embedding
+        st.session_state.embeddings = OllamaEmbeddings(model = "phi3.5")
+        
+        ## Document Ingestion
+        st.session_state.loader = PyPDFDirectoryLoader("research_papers") 
+        
+        ## Document Loading
+        st.session_state.docs = st.session_state.loader.load() 
+        
+        ## Split the documents
+        st.session_state.text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 200)
+        
+        ## Get the final documents
+        st.session_state.final_documents = st.session_state.text_splitter.split_documents(st.session_state.docs[:50])
+        
+        ## Get the vector embeddings
+        st.session_state.vectors = FAISS.from_documents(st.session_state.final_documents, st.session_state.embeddings)
+
+## Title of the app
+st.title("RAG Document Q&A With Groq And Llama3")
+
+## Text input for user query
+user_prompt = st.text_input("Enter your query from the research paper")
+
+## If the user click on the button
+if st.button("Document Embedding"):
+    create_vector_embedding()
+    st.write("Vector Database is ready")
+
+if user_prompt:
+
+    ## Create the document chain
+    document_chain = create_stuff_documents_chain(llm, prompt)
+
+    ## Create the retriever
+    retriever = st.session_state.vectors.as_retriever()
+    
+    ## Create the retrieval chain
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
+
+    start = time.process_time()
+    response = retrieval_chain.invoke({'input': user_prompt})
+    print(f"Response time :{time.process_time()-start}")
+
+    st.write(response['answer'])
+
+    ## With a streamlit expander
+    with st.expander("Document similarity Search"):
+        for i,doc in enumerate(response['context']):
+            st.write(doc.page_content)
+            st.write('------------------------')
+
+
+
+
+
+
